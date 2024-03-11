a9094a3d260704677b3cba22a84c41ac7d86bd0b
Franco42315 <131472256+Franco42315@users.noreply.github.com>
2024-03-10 21:40:57 -0600

    first commit

---
+++ src/compiler/Compiler.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package compiler;
+
+/**
+ *
+ * @author leoma
+ */
+public class Compiler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.setLocationRelativeTo(null);
+        gui.setTitle("Analizador semántico");
+        gui.setVisible(true);
+    }
+    
+}
